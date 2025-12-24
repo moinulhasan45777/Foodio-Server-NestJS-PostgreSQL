85bc66e59e157a5bf00cc9a528ceeeaaf703edd2
moinulhasan45777 <moinul.hasan45777@gmail.com>
2025-12-24 19:20:29 +0600

    Finalizing Incomplete Project

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3001', // allow your Next.js app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
